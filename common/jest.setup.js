8d1a6dac70a96380512ca67bd9d18cd96a110a1a
Seth Orell <seth.orell@gmail.com>
2023-06-24 12:55:22 -0500

    More cleanup

--- common/jest.setup.js
+++ common/jest.setup.js
@@ -10,16 +10,20 @@ const setup = async () => {
   process.env.TABLE_NAME = 'appsync-to-http-products';
   const ordersStackName = 'orders-source-dev';
   const ordersStack = await getStack(ordersStackName);
-  process.env.ORDERS_GRAPH_API_URL = getGraphApiUrl(ordersStack);
-  process.env.ORDERS_GRAPH_API_KEY = getGraphApiKey(ordersStack);
+  process.env.ORDERS_API_URL = getApiUrl(ordersStack);
+  process.env.ORDERS_API_KEY = getApiKey(ordersStack);
   const usersStackName = 'users-source-dev';
   const usersStack = await getStack(usersStackName);
-  process.env.USERS_GRAPH_API_URL = getGraphApiUrl(usersStack);
-  process.env.USERS_GRAPH_API_KEY = getGraphApiKey(usersStack);
+  process.env.USERS_API_URL = getApiUrl(usersStack);
+  process.env.USERS_API_KEY = getApiKey(usersStack);
+  const mergedStackName = 'appsync-merged-dev';
+  const mergedStack = await getStack(mergedStackName);
+  process.env.MERGED_API_URL = getApiUrl(mergedStack);
+  process.env.MERGED_API_KEY = getApiKey(mergedStack);
 };
 
-const getGraphApiUrl = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiUrl')?.OutputValue;
-const getGraphApiKey = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiKey')?.OutputValue;
+const getApiUrl = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiUrl')?.OutputValue;
+const getApiKey = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiKey')?.OutputValue;
 
 const getStack = async (stackName) => {
   const cf = new CloudFormationClient({ region });
