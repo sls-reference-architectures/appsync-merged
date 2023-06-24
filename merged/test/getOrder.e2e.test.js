6f46b09b863e3a1ac1670ea177937baff94ce622
Seth Orell <seth.orell@gmail.com>
2023-06-24 13:46:06 -0500

    Try an api key now

---
+++ merged/test/getOrder.e2e.test.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import retry from 'async-retry';
+
+import { GetOrderQuery } from '../../common/testHelpers';
+
+const BaseUri = process.env.ORDERS_API_URL ?? '';
+
+describe('When getting Order', () => {
+  it('should return Order', async () => {
+    // ARRANGE
+    const requestOptions = {
+      headers: {
+        'x-api-key': process.env.ORDERS_API_KEY ?? '',
+        'Content-Type': 'application/json',
+      },
+      validateStatus: () => true,
+    };
+    const input = { id: 'xyz' };
+
+    await retry(
+      async () => {
+        // ACT
+        const { data, status } = await axios.post(
+          BaseUri,
+          { query: GetOrderQuery, variables: { input } },
+          requestOptions,
+        );
+
+        // ASSERT
+        expect(status).toEqual(200);
+        expect(data.errors).toBeUndefined();
+        expect(data.data.getOrder.quantity).toEqual(42);
+      },
+      { retries: 3 },
+    );
+  });
+});
