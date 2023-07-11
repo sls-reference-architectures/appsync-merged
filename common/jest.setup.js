1a52c307ff5d987d8cee96e377155565f5fc33f2
Seth Orell <seth.orell@gmail.com>
2023-07-11 14:19:31 -0500

    Schema contract test for Orders API

--- common/jest.setup.js
+++ common/jest.setup.js
@@ -12,10 +12,12 @@ const setup = async () => {
   const ordersStack = await getStack(ordersStackName);
   process.env.ORDERS_API_URL = getApiUrl(ordersStack);
   process.env.ORDERS_API_KEY = getApiKey(ordersStack);
+  process.env.ORDERS_API_ID = getApiId(ordersStack);
   const usersStackName = 'users-source-dev';
   const usersStack = await getStack(usersStackName);
   process.env.USERS_API_URL = getApiUrl(usersStack);
   process.env.USERS_API_KEY = getApiKey(usersStack);
+  process.env.USERS_API_ID = getApiId(usersStack);
   const mergedStackName = 'appsync-merged-dev';
   const mergedStack = await getStack(mergedStackName);
   process.env.MERGED_API_URL = getApiUrl(mergedStack);
@@ -24,6 +26,7 @@ const setup = async () => {
 
 const getApiUrl = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiUrl')?.OutputValue;
 const getApiKey = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiKey')?.OutputValue;
+const getApiId = (stack) => stack.Outputs?.find((o) => o.OutputKey === 'ApiId')?.OutputValue;
 
 const getStack = async (stackName) => {
   const cf = new CloudFormationClient({ region });
---
+++ orders/test/schema.int.test.js
@@ -0,0 +1,53 @@
+import { AppSyncClient, GetIntrospectionSchemaCommand } from '@aws-sdk/client-appsync';
+import { diff } from '@graphql-inspector/core';
+import { buildSchema } from 'graphql';
+import Logger from '@dazn/lambda-powertools-logger';
+import fs from 'fs/promises';
+import path from 'path';
+
+describe('GraphQL schema changes', () => {
+  it('should not break existing contract', async () => {
+    // ARRANGE
+    const existingSchema = await getExistingSchema();
+    const newSchema = await getNewSchema();
+
+    // ACT
+    const changes = await diff(existingSchema, newSchema);
+    Logger.debug('changes', { changes });
+
+    // ASSERT
+    expect(hasBreakingChanges(changes)).toBeFalse();
+  });
+});
+
+const getExistingSchema = async () => {
+  const appSyncClient = new AppSyncClient({ region: process.env.AWS_REGION });
+  const input = {
+    apiId: process.env.ORDERS_API_ID,
+    format: 'SDL',
+    includeDirectives: true,
+  };
+  const { schema } = await appSyncClient.send(new GetIntrospectionSchemaCommand(input));
+  const schemaAsText = Buffer.from(schema).toString('utf-8');
+
+  return buildSchema(schemaAsText);
+};
+
+const getNewSchema = async () => {
+  const fileName = 'schema.api.graphql';
+  const filePath = path.join(__dirname, '../', fileName);
+  const schemaAsText = await fs.readFile(filePath, { encoding: 'utf-8' });
+
+  return buildSchema(schemaAsText);
+};
+
+const hasBreakingChanges = (changes) => {
+  let result = false;
+  changes.forEach((change) => {
+    result = result || change.criticality.level === 'BREAKING';
+  });
+
+  Logger.debug('Result', { result });
+
+  return result;
+};
